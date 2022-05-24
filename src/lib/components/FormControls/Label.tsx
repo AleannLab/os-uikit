f28825d0fdd9d8733742d410f0e9e346dd7a7545
Nazar <zaharberku@gmail.com>
2022-05-24 17:23:51 +0300

    edit label kit

--- src/lib/components/FormControls/Label.tsx
+++ src/lib/components/FormControls/Label.tsx
@@ -5,16 +5,19 @@ type Color = 'gray' | 'green' | 'red';
 
 export type LabelProps = ComponentProps<'label'> & {
   color?: Color;
+  isRequired?: boolean;
+  isUp?: boolean;
 };
 
 const colorClasses: Record<Color, string> = {
-  gray: 'text-gray-900 dark:text-gray-300',
+  gray: 'text-gray-400 dark:text-gray-300',
   green: 'text-green-700 dark:text-green-500',
   red: 'text-red-700 dark:text-red-500',
 };
 
-export const Label: FC<LabelProps> = ({ children, color = 'gray', className, ...props }) => (
-  <label className={classNames('text-sm font-medium', colorClasses[color], className)} {...props}>
+export const Label: FC<LabelProps> = ({ children, color = 'gray', className, isUp = false, isRequired, ...props }) => (
+  <label className={classNames('text-sm', colorClasses[color], className, {'uppercase': isUp})} {...props}>
     {children}
+    { isRequired && <span className="inline-flex align-middle ml-1 text-red-600">*</span>}
   </label>
 );
