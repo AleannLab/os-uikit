f0f236e17ba1a569bb17332853d914ad8d511750
Nazar <zaharberku@gmail.com>
2022-05-25 15:37:09 +0300

    add forwardRef

--- src/lib/components/FormControls/Select.tsx
+++ src/lib/components/FormControls/Select.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, FC, ReactNode } from 'react';
+import { ComponentProps, FC, ReactNode, forwardRef } from 'react';
 import classNames from 'classnames';
 
 type Size = 'sm' | 'md' | 'lg' | 'xs';
@@ -17,8 +17,7 @@ export type SelectProps = ComponentProps<'select'> & {
 
 const colorClasses: Record<Color, { select: string; helperText: string }> = {
   base: {
-    select:
-    'bg-white border-gray-200 text-gray-900 placehilder-gray-700 focus:border-blue-600',
+    select: 'bg-white border-gray-200 text-gray-900 placehilder-gray-700 focus:border-blue-600',
     helperText: 'text-gray-500 dark:text-gray-400',
   },
   green: {
@@ -42,43 +41,49 @@ const sizeClasses: Record<Size, string> = {
   lg: 'p-4 text-lg',
 };
 
-export const Select: FC<SelectProps> = ({
-  children,
-  className,
-  sizing = 'md',
-  shadow,
-  helperText,
-  position = 'normal',
-  icon: Icon,
-  color = 'base',
-  full,
-  ...props
-}) => (
-  <>
-    <div className=" relative flex w-full items-center justify-center">
-      {Icon && (
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <Icon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-        </div>
-      )}
-      <select
-        className={classNames(
-          'block rounded border text-base font-normal outline-none disabled:cursor-not-allowed disabled:opacity-50',
-          colorClasses[color].select,
-          sizeClasses[sizing],
-          inputBorderClasses[position],
-          full ? 'w-fit' : 'w-full',
-          {
-            'pl-10': Icon,
-            'shadow-sm dark:shadow-sm-light': shadow,
-          },
-          className,
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  (
+    {
+      children,
+      className,
+      sizing = 'md',
+      shadow,
+      helperText,
+      position = 'normal',
+      icon: Icon,
+      color = 'base',
+      full,
+      ...props
+    },
+    ref,
+  ) => (
+    <>
+      <div className=" relative flex w-full items-center justify-center">
+        {Icon && (
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <Icon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+          </div>
         )}
-        {...props}
-      >
-        {children}
-      </select>
-      {helperText && <p className={classNames('mt-2 text-sm', colorClasses[color].helperText)}>{helperText}</p>}
-    </div>
-  </>
+        <select
+          ref={ref}
+          className={classNames(
+            'block rounded border text-base font-normal outline-none disabled:cursor-not-allowed disabled:opacity-50',
+            colorClasses[color].select,
+            sizeClasses[sizing],
+            inputBorderClasses[position],
+            full ? 'w-fit' : 'w-full',
+            {
+              'pl-10': Icon,
+              'shadow-sm dark:shadow-sm-light': shadow,
+            },
+            className,
+          )}
+          {...props}
+        >
+          {children}
+        </select>
+        {helperText && <p className={classNames('mt-2 text-sm', colorClasses[color].helperText)}>{helperText}</p>}
+      </div>
+    </>
+  ),
 );
