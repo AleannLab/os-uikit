d44f664a0d3d9dfb0c301eeceff88c8a9c9f72e6
Nazar <nazardubovyk@aleannlab.com>
2022-07-08 17:51:11 +0300

    add styles Sidebar

--- src/lib/components/Sidebar/SidebarCollapse.tsx
+++ src/lib/components/Sidebar/SidebarCollapse.tsx
@@ -1,19 +1,26 @@
 import classNames from 'classnames';
 import { FC, PropsWithChildren, useState } from 'react';
-import { HiChevronDown } from 'react-icons/hi';
+import { HiChevronDown, HiChevronUp, HiChevronRight } from 'react-icons/hi';
 import { Tooltip } from '../Tooltip';
 import { useSidebarContext } from './SidebarContext';
 import { SidebarItem } from './SidebarItem';
 import { SidebarItemContext } from './SidebarItemContext';
+import { useSidebarItemContext } from './SidebarItemContext';
 
 export type SidebarCollapseProps = PropsWithChildren<SidebarItem>;
 
 const SidebarCollapse: FC<SidebarCollapseProps> = ({ children, icon: Icon, label, ...rest }) => {
   const { collapsed } = useSidebarContext();
   const [open, setOpen] = useState(false);
+  const { insideCollapse } = useSidebarItemContext();
 
   const Wrapper = ({ children: wrapperChildren }: PropsWithChildren<Record<string, unknown>>) => (
-    <li>
+    <li
+      className={classNames('border-r-transparent', {
+        'border-r-[2px] hover:border-r-[#0577D0]': !insideCollapse,
+        'border-r-[2px] border-r-[#0577D0]': open && !insideCollapse,
+      })}
+    >
       {collapsed ? (
         <Tooltip content={label} placement="right">
           {wrapperChildren}
@@ -27,7 +34,13 @@ const SidebarCollapse: FC<SidebarCollapseProps> = ({ children, icon: Icon, label
   return (
     <Wrapper>
       <button
-        className="group flex w-full items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+        className={classNames(
+          'group flex w-full items-center py-4 pl-6 pr-5 text-sm font-normal text-[#34495E] transition duration-75 hover:bg-[#D8EDFD] hover:font-medium hover:text-[#0577D0]',
+          {
+            'bg-[#D8EDFD] !font-medium !text-[#0577D0]': open,
+            'text-xs !text-[#6C859E] hover:!bg-white hover:font-medium hover:!text-[#0577D0]': !open && insideCollapse,
+          },
+        )}
         data-testid="sidebar-collapse-button"
         onClick={() => setOpen(!open)}
         type="button"
@@ -35,23 +48,34 @@ const SidebarCollapse: FC<SidebarCollapseProps> = ({ children, icon: Icon, label
       >
         {Icon && (
           <Icon
-            className={classNames(
-              'h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white',
-              { 'text-gray-900': open },
-            )}
+            className={classNames('h-5 w-5 text-[#34495E] transition duration-75 group-hover:text-[#0577D0]', {
+              '!text-[#0577D0]': open,
+            })}
           />
         )}
         {collapsed ? (
           <span className="sr-only">{label}</span>
         ) : (
           <>
-            <span className="ml-3 flex-1 whitespace-nowrap text-left">{label}</span>
-            <HiChevronDown className="h-6 w-6" />
+            <span className={classNames('ml-3 whitespace-nowrap text-left', { 'flex-1': !insideCollapse })}>
+              {label}
+            </span>
+            {insideCollapse ? (
+              open ? (
+                <HiChevronDown className="mt-[4px] ml-[7px] h-4 w-4" />
+              ) : (
+                <HiChevronRight className="mt-[4px] ml-[7px] h-4 w-4" />
+              )
+            ) : open ? (
+              <HiChevronUp className="h-5 w-5" />
+            ) : (
+              <HiChevronDown className="h-5 w-5" />
+            )}
           </>
         )}
       </button>
       <ul
-        className={classNames('space-y-2 py-2', {
+        className={classNames({
           hidden: !open,
         })}
         data-testid="sidebar-collapse"
