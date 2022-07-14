c5e8d434b48b53163c0cfd977dd7fb99886ccb8b
astupak <stupak.alexn@gmail.com>
2022-07-14 13:42:07 +0300

    add Button color

--- src/lib/components/Button/index.tsx
+++ src/lib/components/Button/index.tsx
@@ -2,7 +2,7 @@ import { ComponentProps, FC } from 'react';
 import classNames from 'classnames';
 import ButtonGroup from './ButtonGroup';
 
-type Color = 'blue' | 'gray' | 'lightGray' | 'lightGreen' | 'transparent';
+type Color = 'blue' | 'gray' | 'lightGray' | 'lightGreen' | 'transparent' | 'mainRed';
 type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'none';
 type PositionInGroup = 'start' | 'middle' | 'end' | 'normal' | 'rounded';
 type PositionText = 'start' | 'center' | 'end';
@@ -26,6 +26,7 @@ const colorClasses: Record<Color, string> = {
   lightGray: 'text-blue-600 bg-gray-200 hover:bg-gray-400 text-base font-medium disabled:hover:bg-gray-300',
   lightGreen: 'text-white bg-green-400 hover:bg-green-500 text-base font-medium disabled:hover:bg-green-500',
   transparent: 'bg-transparent border-none hover:bg-none',
+  mainRed: 'text-white bg-mainRed hover:opacity-90 text-base font-medium disabled:hover:opacity-70',
 };
 
 const ringClasses: Record<Color, string> = {
@@ -34,6 +35,7 @@ const ringClasses: Record<Color, string> = {
   lightGray: 'text-black bg-white border-gray-400 border hover:bg-gray-200 hover:text-white',
   lightGreen: 'text-black bg-white border-green-400 border hover:bg-green-400 hover:text-white',
   transparent: 'text-black bg-white border-black-700 border hover:bg-black-700 hover:text-white',
+  mainRed: 'text-black bg-white border-mainRed border hover:bg-mainRed hover:text-white',
 };
 
 const sizeClasses: Record<Size, string> = {
