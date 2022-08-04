87bccf033f775a7d9cd220795ad715cd173ce07d
astupak <stupak.alexn@gmail.com>
2022-08-04 13:44:44 +0300

    add ref to FileInput

--- src/lib/components/FormControls/FileInput.tsx
+++ src/lib/components/FormControls/FileInput.tsx
@@ -1,10 +1,10 @@
-import { FC } from 'react';
+import { forwardRef } from 'react';
 import classNames from 'classnames';
 
 import { TextInput, TextInputProps } from './TextInput';
 
 export type FileInputProps = Omit<TextInputProps, 'type'>;
 
-export const FileInput: FC<FileInputProps> = ({ className, ...props }) => {
-  return <TextInput className={classNames('!p-0', className)} {...props} type="file" />;
-};
+export const FileInput: any = forwardRef<HTMLInputElement, FileInputProps>(({ className, ...props }, ref) => (
+  <TextInput ref={ref} className={classNames('!p-0', className)} {...props} type="file" />
+));
