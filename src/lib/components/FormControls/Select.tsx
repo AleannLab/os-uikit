190efa0ed2cee68228ef47bd5458b520ab3c3381
astupak <stupak.alexn@gmail.com>
2022-08-10 16:49:21 +0300

    custom icon for select

--- src/lib/components/FormControls/Select.tsx
+++ src/lib/components/FormControls/Select.tsx
@@ -10,7 +10,8 @@ export type SelectProps = ComponentProps<'select'> & {
   sizing?: Size;
   shadow?: boolean;
   helperText?: ReactNode;
-  isIcon?: boolean;
+  iconName?: string;
+  iconClass?: string;
   position?: Position;
   color?: Color;
   full?: boolean;
@@ -52,7 +53,8 @@ export const Select: any = forwardRef<HTMLSelectElement, SelectProps>(
       shadow,
       helperText,
       position = 'normal',
-      isIcon,
+      iconName,
+      iconClass,
       color = 'base',
       full,
       ...props
@@ -61,9 +63,9 @@ export const Select: any = forwardRef<HTMLSelectElement, SelectProps>(
   ) => (
     <>
       <div className="relative flex w-full items-center justify-center">
-        {isIcon && (
+        {iconName && (
           <div className="pointer-events-none absolute inset-y-0 right-4 flex items-center pl-3">
-            <Icon type={'ArrowDown'} className={'h-4 w-4 stroke-primary-900 text-gray-500'} />
+            <Icon type={iconName} className={iconClass ? iconClass : ''} />
           </div>
         )}
         <select
@@ -75,7 +77,7 @@ export const Select: any = forwardRef<HTMLSelectElement, SelectProps>(
             inputBorderClasses[position],
             full ? 'w-fit' : 'w-full',
             {
-              'pl-10': isIcon,
+              'pl-10': iconName,
               'shadow-sm dark:shadow-sm-light': shadow,
             },
             className,
