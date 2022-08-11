c6d4e5e89d01e2f301ae2a924ba27f3db83f438d
astupak <stupak.alexn@gmail.com>
2022-08-11 16:20:47 +0300

    update Checkbox component

--- src/lib/components/FormControls/Checkbox.tsx
+++ src/lib/components/FormControls/Checkbox.tsx
@@ -1,21 +1,20 @@
 import { ComponentProps, forwardRef, PropsWithChildren } from 'react';
 import classNames from 'classnames';
 
-export type CheckboxProps = Omit<ComponentProps<'input'>, 'type'> | PropsWithChildren<any>;
+export type CheckboxProps = Omit<ComponentProps<'input'>, 'type'> & PropsWithChildren<any>;
 
 export const Checkbox: any = forwardRef<HTMLInputElement, CheckboxProps>(({ className, children, ...props }, ref) => (
-  <>
-    <input
-      ref={ref}
-      className={classNames(
-        'h-4 w-4 rounded border border-gray-300 bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600',
-        className,
-      )}
-      type="checkbox"
-      {...props}
-    />
-    <label htmlFor={props.id} className="ml-2 mb-0 text-sm font-medium text-gray-900 dark:text-gray-300">
-      {children ? children : props.title}
+  <div className={classNames('form-check relative', className)}>
+    <label className="m-0 inline-flex cursor-pointer items-center text-gray-700">
+      <input
+        className="h-4 w-4 !rounded-full border-gray-300 bg-gray-100 text-blue-600 dark:border-gray-600 dark:bg-gray-700"
+        defaultChecked={props.checked}
+        ref={ref}
+        {...props}
+        type="checkbox"
+      />
+      <div className="text-body-1 ml-2 mt-0.5 text-neutral-800">{children ? children : props.title}</div>
     </label>
-  </>
+    {props.tooltip && <p className="absolute -bottom-4 text-xs text-red-600">{props.tooltip}</p>}
+  </div>
 ));
