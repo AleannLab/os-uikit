0e5dcf99f12677b684929893f73663b7f0d949a4
astupak <stupak.alexn@gmail.com>
2023-03-02 10:50:31 +0200

    update avatar style

--- src/lib/components/Avatar/index.tsx
+++ src/lib/components/Avatar/index.tsx
@@ -18,8 +18,16 @@ const sizeClasses: Record<AvatarProps['size'] & string, string> = {
   xs: 'w-6 h-6',
   sm: 'w-8 h-8',
   md: 'w-10 h-10',
-  lg: 'w-20 h-20',
+  lg: 'w-24 h-24',
   xl: 'w-36 h-36',
+};
+
+const statusSizeClasses: Record<AvatarProps['size'] & string, string> = {
+  xs: 'w-1.5 h-1.5',
+  sm: 'w-2.5 h-2.5',
+  md: 'h-3.5 w-3.5',
+  lg: 'w-5 h-5',
+  xl: 'w-8 h-8',
 };
 
 const statusClasses: Record<AvatarProps['status'] & string, string> = {
@@ -29,11 +37,31 @@ const statusClasses: Record<AvatarProps['status'] & string, string> = {
   busy: 'bg-red-400',
 };
 
-const statusPositionClasses: Record<AvatarProps['statusPosition'] & string, string> = {
-  'top-right': '-top-1 -right-1',
-  'top-left': '-top-1 -left-1',
-  'bottom-left': '-bottom-1 -left-1',
-  'bottom-right': '-bottom-1 -right-1',
+const statusPositionValue: Record<AvatarProps['size'] & string, string> = {
+  xs: '1',
+  sm: '1',
+  md: '1',
+  lg: '0',
+  xl: '0',
+};
+
+const statusPositionClasses: Record<AvatarProps['statusPosition'] & string, string[]> = {
+  'top-right': ['-top-', '-right-'],
+  'top-left': ['-top-', '-left-'],
+  'bottom-left': ['-bottom-', '-left-'],
+  'bottom-right': ['-bottom-', '-right-'],
+};
+
+type StatusPosition = 'top-left' | 'top-right' | 'bottom-right' | 'bottom-left';
+
+const getPosition = ({
+  size = 'md',
+  statusPosition = 'top-left',
+}: {
+  size?: keyof typeof statusPositionValue;
+  statusPosition?: StatusPosition;
+}) => {
+  return statusPositionClasses[statusPosition].map((el) => `${el}${statusPositionValue[size]}`);
 };
 
 const AvatarComponent: FC<AvatarProps> = ({
@@ -87,9 +115,10 @@ const AvatarComponent: FC<AvatarProps> = ({
         {status && (
           <span
             className={classNames(
-              'absolute h-3.5 w-3.5 rounded-full border-2 border-white dark:border-gray-800',
+              'absolute rounded-full border-2 border-white dark:border-gray-800',
               statusClasses[status],
-              statusPositionClasses[statusPosition],
+              statusSizeClasses[size],
+              getPosition({ size, statusPosition }),
             )}
           ></span>
         )}
