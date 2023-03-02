e5670bd885d623424c5ee6166a0d51c4c9a8f9bf
astupak <stupak.alexn@gmail.com>
2023-03-02 11:43:26 +0200

    change status size style

--- src/lib/components/Avatar/index.tsx
+++ src/lib/components/Avatar/index.tsx
@@ -27,7 +27,7 @@ const statusSizeClasses: Record<AvatarProps['size'] & string, string> = {
   sm: 'w-2.5 h-2.5',
   md: 'h-3.5 w-3.5',
   lg: 'w-5 h-5',
-  xl: 'w-8 h-8',
+  xl: 'w-7 h-7',
 };
 
 const statusClasses: Record<AvatarProps['status'] & string, string> = {
