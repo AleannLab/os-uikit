076d7ffec69ea71caa8567fc2907bf06e4ea64ec
astupak <stupak.alexn@gmail.com>
2023-03-03 17:34:03 +0200

    update Image with class variable

--- src/lib/components/Image/index.tsx
+++ src/lib/components/Image/index.tsx
@@ -1,17 +1,19 @@
 import { FC } from 'react';
+import classNames from 'classnames';
 
 type ImageProps = {
   image?: string;
   alt?: string;
+  className?: string;
 };
 
-const Image: FC<ImageProps> = ({ image, alt }): JSX.Element | null => {
+const Image: FC<ImageProps> = ({ image, alt, className }): JSX.Element | null => {
   if (!image) {
     return null;
   }
   return (
     <div className="flex items-center justify-center">
-      <img src={image} alt={alt} />
+      <img className={classNames('', className)} src={image} alt={alt} />
     </div>
   );
 };
